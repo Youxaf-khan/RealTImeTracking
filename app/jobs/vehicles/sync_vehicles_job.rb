30c52b9962bd92f0a6bf1f368ac62afcdb377d53
Yousafkhan <YousafKhan611@gmail.com>
2021-09-09 16:03:55 +0500

    Add rescue for sync vehicles job

--- app/jobs/vehicles/sync_vehicles_job.rb
+++ app/jobs/vehicles/sync_vehicles_job.rb
@@ -1,9 +1,12 @@
 module Vehicles
   class SyncVehiclesJob < ApplicationJob
+    class Error < StandardError; end
     queue_as :default
 
     def perform
       Vehicles::UpdateService.call
+    rescue SyncVehiclesJob::Error => e
+      puts "Didn't work because #{e}"
     end
   end
 end
